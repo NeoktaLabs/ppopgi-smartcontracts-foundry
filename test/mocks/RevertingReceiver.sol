2e67042496c7888d4f6ff3814a30912061b4a134
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-01-14 10:10:21 +0100

    Update RevertingReceiver.sol

--- test/mocks/RevertingReceiver.sol
+++ test/mocks/RevertingReceiver.sol
@@ -1,8 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+interface ILotteryFinalize {
+    function finalize() external payable;
+}
+
+interface ILotteryWithdrawNative {
+    function withdrawNativeTo(address to) external;
+}
+
+/// @notice A contract that cannot receive ETH (receive() always reverts),
+/// and can call into the lottery as itself (msg.sender = this contract).
 contract RevertingReceiver {
+    error RejectETH();
+
     receive() external payable {
-        revert("no thanks");
+        revert RejectETH();
+    }
+
+    function callFinalize(address lottery) external payable {
+        ILotteryFinalize(lottery).finalize{value: msg.value}();
+    }
+
+    function callWithdrawNativeTo(address lottery, address to) external {
+        ILotteryWithdrawNative(lottery).withdrawNativeTo(to);
     }
 }
