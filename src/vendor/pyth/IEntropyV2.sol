008f1be9fd0f41f30903947f1d29dd1e3a6c5e4b
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-01-22 12:35:51 +0100

    IEntropyV2

--- src/vendor/pyth/IEntropyV2.sol
+++ src/vendor/pyth/IEntropyV2.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 /// @notice Minimal Entropy interface required by LotterySingleWinner.
-interface IEntropy {
+interface IEntropyV2 {
     function getFee(address provider) external view returns (uint256);
     function requestWithCallback(address provider, bytes32 userRandomness) external payable returns (uint64);
 }
